0ae582f4ee48feb19f9ffb4bc44083187a6e9ca9
atomicnumber2422-code <atomicnumber2422@gmail.com>
2026-02-15 15:00:15 +0800

    Add files via upload

---
+++ data/nodes_Fajardo.js
@@ -0,0 +1,251 @@
+const nodes = [
+//1st floor nodes
+	{
+			id:"Care",
+			name:"Care Center",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 4.84,
+			y: 34.15,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Clinic",
+			name:"Clinic",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 14.62,
+			y: 34.15,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Belcher",
+			name:"8-Belcher",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 26.62,
+			y: 34.15,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Kurzweil",
+			name:"8-Kurzweil",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 41.07,
+			y: 34.15,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Simmons",
+			name:"8-Simmons",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 78.73,
+			y: 34.15,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Planck",
+			name:"9-Planck",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 93.07,
+			y: 34.15,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"stair1_1",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 60.07,
+			y: 34.15,
+			type:"stair",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_1",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 4.84,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_2",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 14.62,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_3",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 26.62,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_4",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 41.07,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_5",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 60.07,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_6",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 78.73,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_7",
+			map:"FAJARDO_FINALBLDG_1.png",
+			x: 93.07,
+			y: 91.86,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+//2nd Floor nodes
+	{
+			id:"Kaist",
+			name:"9-Kaist",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 6.84,
+			y: 36.34,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Fraunhofer",
+			name:"9-Fraunhofer",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 21.18,
+			y: 36.34,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Cern",
+			name:"9-Cern",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 63.62,
+			y: 36.34,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Kavli",
+			name:"9-Kavli",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 78.29,
+			y: 36.34,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Riken",
+			name:"9-Riken",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 92.62,
+			y: 36.34,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"stair2_1",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 42.4,
+			y: 36.34,
+			type:"stair",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_1",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 46.84,
+			y: 89.09,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_2",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 21.18,
+			y: 89.09,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_3",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 42.4,
+			y: 89.09,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_4",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 63.62,
+			y: 89.09,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_5",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 78.29,
+			y: 89.09,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_6",
+			map:"FAJARDO_FINALBLDG_2.png",
+			x: 92.62,
+			y: 89.09,
+			type:"path",
+			floor: 1,
+			selectable: false
+	}
+]
+		
+			

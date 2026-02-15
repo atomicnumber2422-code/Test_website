fb497ffb4680792cce1c9fa36887f1ed26ba847c
atomicnumber2422-code <atomicnumber2422@gmail.com>
2026-02-15 14:44:39 +0800

    Add files via upload

---
+++ data/nodes_SB.js
@@ -0,0 +1,249 @@
+const nodes = [
+//1st floor nodes
+	{
+			id:"Lieber",
+			name:"8-Lieber",
+			map:"SB_FINALBLDG_1.png",
+			x: 23.73,
+			y: 71.28,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"CompLab",
+			name:"Computer Lab",
+			map:"SB_FINALBLDG_1.png",
+			x: 37,4,
+			y: 71.28,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"MT",
+			name:"Master Teacher's Office",
+			map:"SB_FINALBLDG_1.png",
+			x: 45.73,
+			y: 82.5,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Bernoulli",
+			name:"12-Bernoulli",
+			map:"SB_FINALBLDG_1.png",
+			x: 58.73,
+			y: 71.28,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"Shannon",
+			name:"12-Shannon",
+			map:"SB_FINALBLDG_1.png",
+			x: 76.29,
+			y: 71.28,
+			type:"room",
+			floor: 0,
+			selectable: true
+	},
+	{
+			id:"path1_1",
+			map:"SB_FINALBLDG_1.png",
+			x: 7.79,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_2",
+			map:"SB_FINALBLDG_1.png",
+			x: 23.73,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_3",
+			map:"SB_FINALBLDG_1.png",
+			x: 37.4,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_4",
+			map:"SB_FINALBLDG_1.png",
+			x: 45.73,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_5",
+			map:"SB_FINALBLDG_1.png",
+			x: 58.73,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_6",
+			map:"SB_FINALBLDG_1.png",
+			x: 76.29,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path1_7",
+			map:"SB_FINALBLDG_1.png",
+			x: 92.18,
+			y: 26.72,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"stair1_1",
+			map:"SB_FINALBLDG_1.png",
+			x: 7.29,
+			y: 76.56,
+			type:"stair",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"stair1_2",
+			map:"SB_FINALBLDG_1.png",
+			x: 92.18,
+			y: 76.56,
+			type:"stair",
+			floor: 0,
+			selectable: false
+	},
+//2nd Floor nodes
+	{
+			id:"NewComp",
+			name:"New Computer Lab",
+			map:"SB_FINALBLDG_2.png",
+			x: 23.73,
+			y: 71.17,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Stephenson",
+			name:"12-Stephenson",
+			map:"SB_FINALBLDG_2.png",
+			x: 40.62,
+			y: 71.17,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Mayer",
+			name:"12-Mayer",
+			map:"SB_FINALBLDG_2.png",
+			x: 57.84,
+			y: 71.17,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"Pacioli",
+			name:"12-Pacioli",
+			map:"SB_FINALBLDG_2.png",
+			x: 75.51,
+			y: 71.17,
+			type:"room",
+			floor: 1,
+			selectable: true
+	},
+	{
+			id:"path2_1",
+			map:"SB_FINALBLDG_2.png",
+			x: 7.29,
+			y: 27.49,
+			type:"path",
+			floor: 0,
+			selectable: false
+	},
+	{
+			id:"path2_2",
+			map:"SB_FINALBLDG_2.png",
+			x: 23.73,
+			y: 27.49,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_3",
+			map:"SB_FINALBLDG_2.png",
+			x: 40.62,
+			y: 27.49,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_4",
+			map:"SB_FINALBLDG_2.png",
+			x: 57.84,
+			y: 27.49,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_5",
+			map:"SB_FINALBLDG_2.png",
+			x: 75.51,
+			y: 27.49,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"path2_6",
+			map:"SB_FINALBLDG_2.png",
+			x: 92.29,
+			y: 27.49,
+			type:"path",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"stair2_1",
+			map:"SB_FINALBLDG_2.png",
+			x: 92.29,
+			y: 79.58,
+			type:"stair",
+			floor: 1,
+			selectable: false
+	},
+	{
+			id:"stair2_2",
+			map:"SB_FINALBLDG_2.png",
+			x: 7.29,
+			y: 79.58,
+			type:"stair",
+			floor: 1,
+			selectable: false
+	}
+]
+		
+			
